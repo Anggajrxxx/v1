ef0dec4515f7bb10e558cb9fa8966564c8902f0a
Anggajrxxx <141250628+Anggajrxxx@users.noreply.github.com>
2023-09-02 19:24:18 +0700

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -38,24 +38,23 @@ export LIGHT='\033[0;37m'
 export NC='\033[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Mod By ZheeVPN"
-sleep 2
+sleep 1
 clear
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "       ${BIWhite}${UWhite}SETTING ${NC}"
+echo -e "       ${BIWhite}${UWhite}SETTING MENU${NC}"
 echo -e ""
-echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Ganti Domain vps     "
-echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Ganti Port       "
-echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Webmin Menu      "
-echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Speedtest VPS   "
-echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Tentang Script     "
-echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Set Auto Reboot   "
-echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Restart All Service"
-echo -e "     ${BICyan}[${BIWhite}08${BICyan}] Ganti Banner vps"
-echo -e "     ${BICyan}[${BIWhite}09${BICyan}] Cek Bandwith"
-echo -e "     ${BICyan}[${BIWhite}10${BICyan}] Setting Auto Reboot"
+echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Tambah / Ganti Domain "
+echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Ganti Port            "
+echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Webmin Menu           "
+echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Speedtest VPS         "
+echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Tentang Script        "
+echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Set Auto Reboot       "
+echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Restart All Service   "
+echo -e "     ${BICyan}[${BIWhite}08${BICyan}] Ganti Banner SSH      "
+echo -e "     ${BICyan}[${BIWhite}09${BICyan}] Cek Bandwith          "
+echo -e "     ${BICyan}[${BIWhite}10${BICyan}] Setting Auto Reboot   "
 echo -e ""
-echo -e "     ${BICyan}[${BIWhite}00${BiCyan}] Keluar Dari Menu Setting
-
+echo -e "     ${BICyan}[${BIWhite}00${BiCyan}] Keluar Dari Menu Setting"
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
 echo ""
